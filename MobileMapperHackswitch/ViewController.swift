53563fe30e8a7614b92cba8b71b0a5741783a129
Jacob Phipps <houseofphipps@gmail.com>
2023-01-12 11:19:15 -0700

    map

--- MobileMapperHackswitch/ViewController.swift
+++ MobileMapperHackswitch/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+var parks: [MKMapItem] = []
+    
+var currentLocation: CLLocation!
 
+    @IBOutlet weak var MKMapView: MKMapView!
+    
+    @IBAction func whenSearchButtonPressed(_ sender: UIBarButtonItem) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response else { return }
+            for mapItem in response.mapItems {
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.MKMapView.addAnnotation(annotation)
+            }
+        }
+    }
+    @IBAction func whenZoomButtonPressed(_ sender: UIBarButtonItem) {
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        MKMapView.setRegion(region, animated: true)
+    }
+    let locationManager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+    currentLocation = locations[0]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        MKMapView.showsUserLocation = true
+        
     }
-
 
 }
 
