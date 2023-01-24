a80704d4df046e91ab1ed9acaea0e3d8c37df9b9
Jacob Phipps <houseofphipps@gmail.com>
2023-01-24 09:47:07 -0700

    trying to do pin images

--- MobileMapperHackswitch/ViewController.swift
+++ MobileMapperHackswitch/ViewController.swift
@@ -8,13 +8,27 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
-var parks: [MKMapItem] = []
+    let locationManager = CLLocationManager()
     
-var currentLocation: CLLocation!
-
+    var parks: [MKMapItem] = []
+    
+    var currentLocation: CLLocation!
+    
     @IBOutlet weak var MKMapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        MKMapView.showsUserLocation = true
+        
+    }
+    
     
     @IBAction func whenSearchButtonPressed(_ sender: UIBarButtonItem) {
         let request = MKLocalSearch.Request()
@@ -39,22 +53,53 @@ var currentLocation: CLLocation!
         let region = MKCoordinateRegion(center: center, span: coordinateSpan)
         MKMapView.setRegion(region, animated: true)
     }
-    let locationManager = CLLocationManager()
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
     {
-    currentLocation = locations[0]
+        currentLocation = locations[0]
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
-        locationManager.requestWhenInUseAuthorization()
-        MKMapView.showsUserLocation = true
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.isEqual(mapView.userLocation) {
+            return nil
+        }
+        let pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.image = UIImage(named: "MobileMakerIconPinImage")
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        
+        return pin
+        
         
     }
-
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
+                 calloutAccessoryControlTapped control: UIControl) {
+        var currentMapItem = MKMapItem()
+        if let coordinate = view.annotation?.coordinate {
+            for mapitem in parks {
+                if mapitem.placemark.coordinate.latitude == coordinate.latitude &&
+                    
+                    mapitem.placemark.coordinate.longitude == coordinate.longitude {
+                    
+                    currentMapItem = mapitem
+                }
+            }
+        }
+        let placemark = currentMapItem.placemark
+        print(currentMapItem)
+        if let parkName = placemark.name, let streetNumber = placemark.subThoroughfare, let streetName =
+            placemark.thoroughfare
+        {
+            
+            let streetAddress = streetNumber + " " + streetName
+            
+            let alert = UIAlertController(title: parkName, message: streetAddress, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        
+        
+    }
+    
 }
-
